5959b591c9c3e191f0063b7f353fb8e71c853ebd
sanket <sankrtpowar@gmail.com>
2024-06-02 22:52:51 -0700

    Amazing Mouse Effect- HTML CSS JS- Sheryians Coding Shcool

---
+++ script.js
@@ -0,0 +1,45 @@
+const throttleFunction = (func, delay) => {
+  let prev = 0;
+  return (...args) => {
+    let now = new Date().getTime();
+    if (now - prev > delay) {
+      prev = now;
+      return func(...args);
+    }
+  }
+}
+
+
+document.querySelector("#contener").addEventListener("mousemove", throttleFunction((dit) => {
+  var div = document.createElement("div")
+  div.classList.add("imgdiv")
+
+  div.style.left = dit.clientX - 100 + 'px'
+  div.style.top = dit.clientY - 280 + 'px'
+
+
+var img = document.createElement("img")
+img.setAttribute("src", "./project_20220729_1711234-01_AqKEbRv65-removebg-preview.png")
+div.appendChild(img)
+document.body.appendChild(div)
+
+
+
+gsap.to(img, {
+  y: "0",
+  ease: Power4,
+  duration: .5,
+})
+
+gsap.to(img, {
+  y: "100%",
+  delay: .6,
+  ease: Power2,
+})
+
+setTimeout(function () {
+  div.remove()
+},1500)
+},170));
+
+
